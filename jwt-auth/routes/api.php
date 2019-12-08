55a5565aca0a7ecf7d29d3efc9216bc672aae46a
Nando Hidayat <nando@lenovo>
2019-12-09 02:24:00 +0700

    api auth

--- jwt-auth/routes/api.php
+++ jwt-auth/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,11 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+Route::post('/register', 'Auth\RegisterController@register');
+Route::post('/login', 'Auth\RegisterController@login');
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
